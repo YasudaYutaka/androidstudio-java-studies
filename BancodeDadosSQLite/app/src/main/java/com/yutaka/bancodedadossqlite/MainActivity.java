fe28114eb2dae37f12a29893aa8d1027635b51e2
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2020-09-01 15:29:56 -0300

    Banco de Dados SQLITE

--- BancodeDadosSQLite/app/src/main/java/com/yutaka/bancodedadossqlite/MainActivity.java
+++ BancodeDadosSQLite/app/src/main/java/com/yutaka/bancodedadossqlite/MainActivity.java
@@ -20,24 +20,60 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
             // Criar tabela
-            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT(3)) ");
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INT(3)) ");
+            //bancoDados.execSQL("DROP TABLE pessoas");
 
             // Inserir dados
-            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Yutaka', 20) "); // desse modo vai toda hora adicionar
-            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Tanaka', 22) ");
+            //bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Pedro', 15) "); // desse modo vai toda hora adicionar
+            //bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('João', 54) ");
+
+            // Update
+            //bancoDados.execSQL("UPDATE pessoas SET idade = 10, nome = 'Lucianna' WHERE id = 1");
+
+            // DELETAR
+            //bancoDados.execSQL("DELETE FROM pessoas WHERE id = 3");
 
             // Recuperar pessoas
-            Cursor cursor = bancoDados.rawQuery("SELECT nome, idade FROM pessoas", null);
+            /*String consulta =
+                    "SELECT nome, idade FROM pessoas " +
+                    "WHERE nome = 'Yutaka' AND idade = 20 "; */
+
+            /*String consulta =
+                    "SELECT nome, idade FROM pessoas " +
+                            "WHERE idade >= 25 OR idade = 20 "; */
+
+            /*String consulta =
+                    "SELECT nome, idade FROM pessoas " +
+                            "WHERE idade IN(18,55) "; */
+
+            /*String consulta =
+                    "SELECT nome, idade FROM pessoas " +
+                            "WHERE idade BETWEEN 20 AND 55"; */
+
+            /*String consulta =
+                    "SELECT nome, idade FROM pessoas " +
+                            "WHERE nome LIKE '%ta%' "; // %ta -> tem letras antes de ta   ta% tem ta e letras dps */
+
+            String consulta =
+                    "SELECT * FROM pessoas " +
+                            "WHERE 1=1 ORDER BY idade ASC"; // Ordena de forma ascendente ou descendente ASC / DESC /// LIMIT -> limita quantidade que aparece
+
+            Cursor cursor = bancoDados.rawQuery(consulta, null);
 
             // indices da tabela
+            int indiceId = cursor.getColumnIndex("id");
             int indiceNome = cursor.getColumnIndex("nome");
             int indiceIdade = cursor.getColumnIndex("idade");
 
             cursor.moveToFirst();
 
             while (cursor != null) {
-                Log.i("RESULTADO - nome: ", cursor.getString(indiceNome));
-                Log.i("RESULTADO - idade: ", cursor.getString(indiceIdade));
+
+                String id = cursor.getString(indiceId);
+                String nome = cursor.getString(indiceNome);
+                String idade = cursor.getString(indiceIdade);
+
+                Log.i("RESULTADO - id ", id + " / nome: " + nome + " / idade: " + idade);
                 cursor.moveToNext();
             }
 
