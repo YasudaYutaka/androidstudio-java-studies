0c8d3acae4301e1efa0c2f3b777472f815a5fa9c
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2020-09-17 15:12:32 -0300

    Organizze clone - Exibição do saldo, calendário

--- Organizzeclone/app/src/main/java/com/yutaka/organizzeclone/activity/PrincipalActivity.java
+++ Organizzeclone/app/src/main/java/com/yutaka/organizzeclone/activity/PrincipalActivity.java
@@ -6,21 +6,54 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 import com.yutaka.organizzeclone.R;
+import com.yutaka.organizzeclone.config.ConfiguracaoFirebase;
+import com.yutaka.organizzeclone.helper.Base64Custom;
+import com.yutaka.organizzeclone.model.Usuario;
+
+import java.text.DecimalFormat;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private MaterialCalendarView calendarView;
+    private TextView textSaudacao, textSaldo;
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private Double despesaTotal = 0.00;
+    private Double receitaTotal = 0.00;
+    private Double resumoUsuario = 0.00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
+
+        textSaudacao = findViewById(R.id.textViewSaudacao);
+        textSaldo = findViewById(R.id.textViewSaldo);
+        calendarView = findViewById(R.id.calendarView);
+        configuraCalendarView();
+        recuperarResumo();
+
 
         /*
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -33,12 +66,76 @@ public class PrincipalActivity extends AppCompatActivity {
         }); */
     }
 
+    public void recuperarResumo() {
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Usuario usuario = dataSnapshot.getValue(Usuario.class);
+
+                despesaTotal = usuario.getDespesaTotal();
+                receitaTotal = usuario.getReceitaTotal();
+                resumoUsuario = receitaTotal - despesaTotal;
+
+                DecimalFormat decimalFormat = new DecimalFormat("0.##"); // # caso seja 0 n exibe
+                String resultadoFormatado = decimalFormat.format(resumoUsuario);
+
+                textSaudacao.setText("Olá, " + usuario.getNome());
+                textSaldo.setText("R$ " + resultadoFormatado);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuSair:
+                autenticacao.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void adicionarDespesa(View view) {
         startActivity(new Intent(this, DespesasActivity.class));
     }
 
     public void adicionarReceita(View view) {
         startActivity(new Intent(this, ReceitasActivity.class));
+    }
+
+    public void configuraCalendarView() {
+
+        CharSequence meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
+
     }
 
 }
