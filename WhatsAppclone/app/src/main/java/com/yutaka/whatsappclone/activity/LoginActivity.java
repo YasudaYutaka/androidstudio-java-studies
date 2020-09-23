3477583c4352edcdaff005b628266cbfbdf7eb90
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2020-09-23 14:29:10 -0300

    Login de usuario feito (FirebaseAuth)

--- WhatsAppclone/app/src/main/java/com/yutaka/whatsappclone/activity/LoginActivity.java
+++ WhatsAppclone/app/src/main/java/com/yutaka/whatsappclone/activity/LoginActivity.java
@@ -1,23 +1,97 @@
 package com.yutaka.whatsappclone.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.yutaka.whatsappclone.R;
+import com.yutaka.whatsappclone.config.ConfiguracaoFirebase;
+import com.yutaka.whatsappclone.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText campoEmail, campoSenha;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        campoEmail = findViewById(R.id.editEmail);
+        campoSenha = findViewById(R.id.editSenha);
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+
     }
 
     public void abrirTelaCadastro(View view) {
         startActivity(new Intent(LoginActivity.this, CadastroActivity.class));
+    }
+
+    public void abrirTelaPrincipal() {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+    }
+
+    public void logarUsuario(Usuario usuario) {
+
+        autenticacao.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Sucesso ao fazer o login!", Toast.LENGTH_SHORT).show();
+                    abrirTelaPrincipal();
+                } else {
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidUserException e) {
+                        excecao = "Usuário não está cadastrado.";
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        excecao = "E-mail e senha não correspondem a um usuário cadastrado.";
+                    } catch (Exception e) {
+                        excecao = "Erro ao logar o usuário: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this, excecao, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    public void validarLoginUsuario(View view) {
+
+        String textoEmail = campoEmail.getText().toString();
+        String textoSenha = campoSenha.getText().toString();
+
+        if(!textoEmail.isEmpty()) {
+            if(!textoSenha.isEmpty()) {
+                Usuario usuario = new Usuario();
+                usuario.setEmail(textoEmail);
+                usuario.setSenha(textoSenha);
+
+                logarUsuario(usuario);
+            } else {
+                Toast.makeText(LoginActivity.this, "Digite sua senha!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(LoginActivity.this, "Digite seu e-mail!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
